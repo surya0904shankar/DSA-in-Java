81414e677a52dfcb825b1f88f53c4316457e57e7
surya0904shankar <suryas0904@gmail.com>
2025-04-26 16:34:32 +0530

    Rotate by N places implemented successfully

--- com.array/src/RotatebyDPlaces.java
+++ com.array/src/RotatebyDPlaces.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class RotatebyDPlaces {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        int n;
+        System.out.println("Enter the value of d");
+        n = sc.nextInt();
+        RotatebyDPlaces(arr, n);
+    }
+
+    public static void RotatebyDPlaces(int[] arr, int n) {
+
+    }
 }
