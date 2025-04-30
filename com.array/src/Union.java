f1955f93633f901921a5e3803b5d21fb9ac4cb55
surya0904shankar <suryas0904@gmail.com>
2025-04-30 23:20:55 +0530

    New commit in union.java

--- com.array/src/Union.java
+++ com.array/src/Union.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Union {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[8];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        int[] arr1 = new int[8];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        int[] arr2 = new int[8];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = sc.nextInt();
         }
     }
 }
