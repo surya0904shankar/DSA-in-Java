86687f9694fb9435af92fe2c30ecb3db99b164b6
surya0904shankar <suryas0904@gmail.com>
2025-04-30 23:19:49 +0530

    New commit in union.java

--- com.array/src/Union.java
+++ com.array/src/Union.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Union {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
 }
